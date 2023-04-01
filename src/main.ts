bdaba87846f09696e87ff5b1f948f1096718feae
Maxime Verreault <maxime.verreault@ka-group.com>
2023-03-31 20:44:14 -0400

    Fixed Prettier on files after change to .prettierrc.json file (2 space tabs to 4 space tabs)

--- src/main.ts
+++ src/main.ts
@@ -7,113 +7,133 @@ import * as tc from '@actions/tool-cache'
 import {major, minor} from 'semver'
 
 async function run(): Promise<void> {
-  try {
-    const directory: string =
-      core.getInput('directory') || `${process.env['RUNNER_TEMP']}/fmt`
-    const installDir = `${directory}/install`
     try {
-      await io.mkdirP(directory)
-    } catch (e) {
-      core.setFailed('Failed to create FMT working directory')
-    }
-    let version: string = core.getInput('version') || 'latest'
-    const latest = version === 'latest'
-    core.info(`Fetching FMT version: ${version} ...`)
-    core.debug(`FMT working directory: ${directory}`)
-    if (latest) {
-      const md = await exec.getExecOutput('gh', [
-        'release',
-        'view',
-        '-R',
-        'fmtlib/fmt'
-      ])
-      version = md.stdout.slice(
-        md.stdout.indexOf('\t') + 1,
-        md.stdout.indexOf('\n')
-      )
-    }
-    const paths = [directory]
-    const key = `fmt-${version}`
-    const restoreKeys = [
-      `fmt-${major(version)}.${minor(version)}`,
-      `fmt-${major(version)}`,
-      'fmt-'
-    ]
-    const cacheKey = await cache.restoreCache(paths, key, restoreKeys)
-    if (!cacheKey) {
-      core.debug('FMT not found in cache')
-      if (latest) {
-        const ret2 = await exec.exec(
-          'gh',
-          [
-            'release',
-            'download',
-            '-R',
-            'fmtlib/fmt',
-            '--clobber',
-            '--pattern',
-            'fmt*.zip'
-          ],
-          {
-            cwd: directory
-          }
-        )
-        if (ret2 !== 0) {
-          core.setFailed(`Could not download the ${version} release for FMT`)
+        const directory: string =
+            core.getInput('directory') || `${process.env['RUNNER_TEMP']}/fmt`
+        const installDir = `${directory}/install`
+        try {
+            await io.mkdirP(directory)
+        } catch (e) {
+            core.setFailed('Failed to create FMT working directory')
         }
-      } else {
-        const ret = await exec.exec(
-          'gh',
-          ['release', 'download', version, '--clobber', '-R', 'fmtlib/fmt'],
-          {
-            cwd: directory
-          }
-        )
-        if (ret !== 0) {
-          core.setFailed(`Could not download the ${version} release for FMT`)
+        let version: string = core.getInput('version') || 'latest'
+        const latest = version === 'latest'
+        core.info(`Fetching FMT version: ${version} ...`)
+        core.debug(`FMT working directory: ${directory}`)
+        if (latest) {
+            const md = await exec.getExecOutput('gh', [
+                'release',
+                'view',
+                '-R',
+                'fmtlib/fmt'
+            ])
+            version = md.stdout.slice(
+                md.stdout.indexOf('\t') + 1,
+                md.stdout.indexOf('\n')
+            )
         }
-      }
-      const fmtZipPattern = await glob.create(`${directory}/*.zip`, {
-        followSymbolicLinks: false,
-        matchDirectories: false
-      })
-      const zipFile = await fmtZipPattern.glob()
-      const fileSavePath = await tc.extractZip(zipFile[0], directory)
-      core.debug(`Extracted FMT to ${fileSavePath}`)
+        const paths = [directory]
+        const key = `fmt-${version}`
+        const restoreKeys = [
+            `fmt-${major(version)}.${minor(version)}`,
+            `fmt-${major(version)}`,
+            'fmt-'
+        ]
+        const cacheKey = await cache.restoreCache(paths, key, restoreKeys)
+        if (!cacheKey) {
+            core.debug('FMT not found in cache')
+            if (latest) {
+                const ret2 = await exec.exec(
+                    'gh',
+                    [
+                        'release',
+                        'download',
+                        '-R',
+                        'fmtlib/fmt',
+                        '--clobber',
+                        '--pattern',
+                        'fmt*.zip'
+                    ],
+                    {
+                        cwd: directory
+                    }
+                )
+                if (ret2 !== 0) {
+                    core.setFailed(
+                        `Could not download the ${version} release for FMT`
+                    )
+                }
+            } else {
+                const ret = await exec.exec(
+                    'gh',
+                    [
+                        'release',
+                        'download',
+                        version,
+                        '--clobber',
+                        '-R',
+                        'fmtlib/fmt'
+                    ],
+                    {
+                        cwd: directory
+                    }
+                )
+                if (ret !== 0) {
+                    core.setFailed(
+                        `Could not download the ${version} release for FMT`
+                    )
+                }
+            }
+            const fmtZipPattern = await glob.create(`${directory}/*.zip`, {
+                followSymbolicLinks: false,
+                matchDirectories: false
+            })
+            const zipFile = await fmtZipPattern.glob()
+            const fileSavePath = await tc.extractZip(zipFile[0], directory)
+            core.debug(`Extracted FMT to ${fileSavePath}`)
 
-      const fmtFolderPattern = await glob.create(`${directory}/fmt*/`, {
-        followSymbolicLinks: false,
-        matchDirectories: true,
-        implicitDescendants: false
-      })
-      const fmtFolderList = await fmtFolderPattern.glob()
-      const fmtFolder = fmtFolderList[0]
-      core.debug(`FMT extracted folder path: ${fmtFolder}`)
-      const toolset: string = core.getInput('toolset')
-      // const platform: string = core.getInput('platform')
+            const fmtFolderPattern = await glob.create(`${directory}/fmt*/`, {
+                followSymbolicLinks: false,
+                matchDirectories: true,
+                implicitDescendants: false
+            })
+            const fmtFolderList = await fmtFolderPattern.glob()
+            const fmtFolder = fmtFolderList[0]
+            core.debug(`FMT extracted folder path: ${fmtFolder}`)
+            const toolset: string = core.getInput('toolset')
+            // const platform: string = core.getInput('platform')
 
-      await io.mkdirP(`${fmtFolder}/build`)
-      const args =
-        toolset !== 'auto'
-          ? ['..', '-G', toolset, `-DCMAKE_INSTALL_PREFIX:PATH=${installDir}`]
-          : ['..', `-DCMAKE_INSTALL_PREFIX:PATH=${installDir}`]
-      await exec.exec('cmake', args, {
-        cwd: `${fmtFolder}/build`
-      })
-      await exec.exec('cmake', ['--build', 'build', '--target', 'install'], {
-        cwd: fmtFolder
-      })
-      const cacheId = await cache.saveCache(paths, key)
-      if (cacheId) {
-        core.debug(`Successfully saved with cache ID ${cacheId}`)
-      }
-    } else {
-      core.debug(`Found FMT with key ${cacheKey}`)
+            await io.mkdirP(`${fmtFolder}/build`)
+            const args =
+                toolset !== 'auto'
+                    ? [
+                          '..',
+                          '-G',
+                          toolset,
+                          `-DCMAKE_INSTALL_PREFIX:PATH=${installDir}`
+                      ]
+                    : ['..', `-DCMAKE_INSTALL_PREFIX:PATH=${installDir}`]
+            await exec.exec('cmake', args, {
+                cwd: `${fmtFolder}/build`
+            })
+            await exec.exec(
+                'cmake',
+                ['--build', 'build', '--target', 'install'],
+                {
+                    cwd: fmtFolder
+                }
+            )
+            const cacheId = await cache.saveCache(paths, key)
+            if (cacheId) {
+                core.debug(`Successfully saved with cache ID ${cacheId}`)
+            }
+        } else {
+            core.debug(`Found FMT with key ${cacheKey}`)
+        }
+        core.addPath(installDir)
+    } catch (error) {
+        if (error instanceof Error) core.setFailed(error.message)
     }
-    core.addPath(installDir)
-  } catch (error) {
-    if (error instanceof Error) core.setFailed(error.message)
-  }
 }
 
 run()
