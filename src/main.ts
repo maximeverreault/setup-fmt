2ecf2ad3906a3a93e39c9799233501f943c9714f
Maxime Verreault <maximeverreault24@hey.com>
2023-04-01 14:45:18 -0400

    Added fetching, extracting and installing the FMT package based on action inputs

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,78 @@
 import * as core from '@actions/core'
-import * as fs from 'fs'
-import * as httpm from '@actions/http-client'
-import path from 'path'
+import * as exec from '@actions/exec'
+import * as glob from '@actions/glob'
+import * as io from '@actions/io'
+import * as tc from '@actions/tool-cache'
 
 async function run(): Promise<void> {
   try {
-    const version: string = core.getInput('version') || '9.1.0'
-    // const toolset: string = core.getInput('toolset')
-    // const platform: string = core.getInput('platform')
     const directory: string =
       core.getInput('directory') || `${process.env['RUNNER_TEMP']}/fmt`
-    const sampleFilePath: string = path.join(directory, 'fmt-9.1.0.zip')
-    core.debug(`Fetching FMT v${version} ...`)
-    const file = fs.createWriteStream(sampleFilePath)
-    const client = new httpm.HttpClient()
-    const {message} = await client.get(
-      `https://github.com/fmtlib/fmt/releases/download/${version}/fmt-${version}.zip`
-    )
-    message.pipe(file)
+    try {
+      await io.mkdirP(directory)
+    } catch (e) {
+      core.error('Failed to create FMT working directory')
+    }
+    const version: string = core.getInput('version') || '9.1.0'
+    core.info(`Fetching FMT version: ${version} ...`)
+    core.debug(`FMT working directory: ${directory}`)
+    if (version === 'latest') {
+      const ret = await exec.exec(
+        'gh',
+        [
+          'release',
+          'download',
+          '-R',
+          'fmtlib/fmt',
+          '--clobber',
+          '--pattern',
+          `'*.zip'`
+        ],
+        {
+          cwd: directory
+        }
+      )
+      if (ret !== 0) {
+        core.setFailed(`Could not download the ${version} release for FMT`)
+      }
+    } else {
+      const ret = await exec.exec(
+        'gh',
+        ['release', 'download', version, '--clobber', '-R', 'fmtlib/fmt'],
+        {
+          cwd: directory
+        }
+      )
+      if (ret !== 0) {
+        core.setFailed(`Could not download the ${version} release for FMT`)
+      }
+    }
+    const fmtZipPattern = await glob.create(`${directory}/*.zip`, {
+      followSymbolicLinks: false,
+      matchDirectories: false
+    })
+    const zipFile = await fmtZipPattern.glob()
+    const fileSavePath = await tc.extractZip(zipFile[0], directory)
+    core.debug(`Extracted FMT to ${fileSavePath}`)
+
+    const fmtFolderPattern = await glob.create(`${directory}/fmt*`, {
+      followSymbolicLinks: false,
+      matchDirectories: true,
+      implicitDescendants: false
+    })
+    const fmtFolderList = await fmtFolderPattern.glob()
+    const fmtFolder = fmtFolderList[0]
+    core.debug(`FMT extracted folder path: ${fmtFolder}`)
+    const toolset: string = core.getInput('toolset')
+    // const platform: string = core.getInput('platform')
+
+    await io.mkdirP(`${fmtFolder}/build`)
+    await exec.exec('cmake', ['..', '-G', toolset], {
+      cwd: `${fmtFolder}/build`
+    })
+    await exec.exec('cmake', ['--build', 'build', '--target', 'install'], {
+      cwd: fmtFolder
+    })
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
