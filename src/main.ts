0b9aaa292118af1574b40b60b89575b943065b35
Maxime Verreault <maximeverreault24@hey.com>
2023-04-01 14:45:17 -0400

    Initial changes for fetch of zip

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as fs from 'fs'
+import * as httpm from '@actions/http-client'
+import path from 'path'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    const version: string = core.getInput('version') || '9.1.0'
+    // const toolset: string = core.getInput('toolset')
+    // const platform: string = core.getInput('platform')
+    const directory: string =
+      core.getInput('directory') || `${process.env['RUNNER_TEMP']}/fmt`
+    const sampleFilePath: string = path.join(directory, 'fmt-9.1.0.zip')
+    core.debug(`Fetching FMT v${version} ...`)
+    const file = fs.createWriteStream(sampleFilePath)
+    const client = new httpm.HttpClient()
+    const {message} = await client.get(
+      `https://github.com/fmtlib/fmt/releases/download/${version}/fmt-${version}.zip`
+    )
+    message.pipe(file)
 
     core.setOutput('time', new Date().toTimeString())
   } catch (error) {
